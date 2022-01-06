03f0062f825269a7cd530e540e3d326ea02d5544
AV-Felipe <aerogrado@hotmail.com>
2022-01-06 17:52:13 -0300

    Sort and arrange project functions

--- pages/script-v2.js
+++ pages/script-v2.js
@@ -6,20 +6,51 @@ const inputFieldKind = document.getElementById('queryFieldKind');
 
 // EVENT LISTENERS
 inputField.addEventListener('input', startQuerying);
+inputFieldKind.addEventListener('change', checkInputValue);
 
 // GLOBAL VARIABLES
 let inputValue = 0;
 
-// SERVER COMUNICATION
+// FUNCTIONS
 
-//search value treatment
+//delays the execution of the live query to accept more input
+async function startQuerying(){
+
+    inputField.removeEventListener('input', startQuerying); //prevents multiple callback executions
+    
+    let initialValue = inputValue;
+    console.log('valor inicial no campo de pesquisa:' + initialValue);
+
+    await new Promise((resolve)=>{
+        setTimeout(resolve, 2000);
+    })
+
+    if (initialValue != inputField.value){
+        checkInputValue();
+    }
+
+    inputField.addEventListener('input', startQuerying);
+
+}
+
+
+//prepares input value before sending it to the server
 function checkInputValue() {
-    displayResults.innerHTML = "";
 
+    //clear any output value
+    displayResults.innerHTML = "";
+    alertMessageHandler('remove');
+
+    //set used variables
     inputValue = inputField.value;
+    if(inputValue === ""){
+        return; //stop searching if search box is empty
+    }
     let inputValueType = inputFieldKind.value;
 
+    //validates value according to desired search field rules
     if(inputValueType === 'id'){
+
         if(Number.isInteger(Number(inputValue))){
             console.log('procurando por id:' + inputValue);
             //return inputValue;
@@ -27,38 +58,36 @@ function checkInputValue() {
             alertMessageHandler('add', 'id');
             return 'invalid input value on search field';
         }
+
     }else if(inputValueType === 'name'){
+
         if(countNumbersInString(inputValue)){
             alertMessageHandler('add', 'name');
             return 'invalid input value on search field';
+        }else if(inputValue.length <= 3){
+            alertMessageHandler('add', 'short');
+            return 'input value on search field is to short';
         }else{
             console.log('procurando por nome:' + inputValue);
             //return inputValue;
         }
-    }else{
-        console.log('procurando por e-mail:' + inputValue);
-        //return inputValue;
+
+    }else{//residual rule, for e-mail
+
+        if(inputValue.length <= 3){
+            alertMessageHandler('add', 'short');
+            return 'input value on search field is to short';
+        }else{
+            console.log('procurando por e-mail:' + inputValue);
+            //return inputValue;
+        }
+        
     }
 
     sendQueryToServer(inputValue, inputValueType);
 
 }
 
-//delay function for live search execution
-async function inputCheckDelay(calbackFunction) {
-    let initialValue = inputValue;
-    console.log(initialValue);
-    alertMessageHandler('remove');
-
-    await new Promise((resolve)=>{
-        setTimeout(resolve, 2000);
-    })
-
-    if(inputField.value !== initialValue){
-        calbackFunction();
-    }else{console.log('nada')}
-    
-}
 
 //error messages for search value field validadtion
 function alertMessageHandler(operation, type="") {
@@ -84,6 +113,7 @@ function alertMessageHandler(operation, type="") {
     
 }
 
+
 //count numbers in string
 function countNumbersInString(stringValue){
     //use a regular expression to cycle through all string characters and return an array of numbers, if any
@@ -96,26 +126,8 @@ function countNumbersInString(stringValue){
     }
 }
 
-//activates function when input lenght is biger than 3 characters
-async function startQuerying(){
-    inputField.removeEventListener('input', startQuerying);
-    alertMessageHandler('remove');
-    
-    let initialValue = inputValue;
-    console.log('valor inicial no campo de pesquisa:' + initialValue);
 
-    await new Promise((resolve)=>{
-        setTimeout(resolve, 2000);
-    })
-
-    if (initialValue != inputField.value){
-        checkInputValue();
-    }
-
-    inputField.addEventListener('input', startQuerying);
-
-    
-}
+// SERVER COMMUNICATION
 
 //fetch content from server
 
