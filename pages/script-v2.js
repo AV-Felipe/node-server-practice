d80e1629041491a07ad2fb05d7578a44ae249306
AV-Felipe <aerogrado@hotmail.com>
2022-01-06 14:51:27 -0300

    Implement basic live search

--- pages/script-v2.js
+++ pages/script-v2.js
@@ -14,26 +14,33 @@ let inputValue = 0;
 
 //search value treatment
 function checkInputValue() {
+    displayResults.innerHTML = "";
+
     inputValue = inputField.value;
     let inputValueType = inputFieldKind.value;
 
     if(inputValueType === 'id'){
         if(Number.isInteger(Number(inputValue))){
-            console.log(inputValue);
-            return inputValue;
+            console.log('procurando por id:' + inputValue);
+            //return inputValue;
         }else{
             alertMessageHandler('add', 'id');
-            //return 'invalid input value on search field';
+            return 'invalid input value on search field';
         }
     }else if(inputValueType === 'name'){
-        if(countNumbersInString()){
+        if(countNumbersInString(inputValue)){
             alertMessageHandler('add', 'name');
+            return 'invalid input value on search field';
         }else{
-            console.log(inputValue);
-            return inputValue;
+            console.log('procurando por nome:' + inputValue);
+            //return inputValue;
         }
+    }else{
+        console.log('procurando por e-mail:' + inputValue);
+        //return inputValue;
     }
 
+    sendQueryToServer(inputValue, inputValueType);
 
 }
 
@@ -90,8 +97,60 @@ function countNumbersInString(stringValue){
 }
 
 //activates function when input lenght is biger than 3 characters
-function startQuerying(){
-    //if (inputField.value.length > 3){
-        inputCheckDelay(checkInputValue);
-    //}
+async function startQuerying(){
+    inputField.removeEventListener('input', startQuerying);
+    alertMessageHandler('remove');
+    
+    let initialValue = inputValue;
+    console.log('valor inicial no campo de pesquisa:' + initialValue);
+
+    await new Promise((resolve)=>{
+        setTimeout(resolve, 2000);
+    })
+
+    if (initialValue != inputField.value){
+        checkInputValue();
+    }
+
+    inputField.addEventListener('input', startQuerying);
+
+    
+}
+
+//fetch content from server
+
+function sendQueryToServer(searchString, searchClass) {
+
+    fetch(`/stock/?value=${searchString}&field=${searchClass}`)
+    .then(
+        function(response){
+            if(response.status !== 200){
+                console.log('deu zica. Status code da resposta:' + response.status);
+                return;
+            }
+
+            response.json().then(function(data){
+                console.log(data);
+                data.forEach(e => {
+                    if(e.id !== undefined){
+                        document.getElementById("displayResult").innerHTML += generateTableFields(e.id, e.name, e.email);
+                    }
+                });
+            });
+        }
+    )
+    .catch(function(err){
+        console.log('Fetch Error :-S', err);
+    });
+
+}
+
+function generateTableFields (id, name, email) {
+    return `
+        <tr>
+            <td>${id}</td>
+            <td>${name}</td>
+            <td>${email}</td>
+        </tr>
+        `;
 }
